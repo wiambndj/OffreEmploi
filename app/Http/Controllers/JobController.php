a7226c354bb79fa750d868ca52a3ea75eca007e1
Narimane Benyoub <benyounnarimane@gmail.com>
2020-01-08 23:26:12 +0100

    ajouter des fontions dans le jobcontrolleur

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -39,4 +39,99 @@ class JobController extends Controller
         $job->save();
                     
      }
+       public function index1(){
+            $users = DB::table('users')
+            ->join('companies', 'users.id', '=', 'companies.user_id')
+            ->join('jobs', 'jobs.user_id', '=', 'companies.id')->where('users.id', '=',Auth::user()->id) ->paginate(5);
+        
+        return view('offrelist', ['offres' => $users]);
+     
+        
+    }
+    public function index2(){
+        $users = DB::table('companies')
+
+        ->join('jobs', 'jobs.user_id', '=', 'companies.id')->paginate(5);
+    
+    return view('joblist', ['offres' => $users]);
+ 
+    
+}
+    public function edit($id) {
+
+        $job = Job::find($id);
+ 
+       
+ 
+        return view('offre', ['job' => $job]);
+         
+     }
+     
+     //permet de modifier un cv
+     public function update(Request $request, $id) {
+         
+         $job = Job::find($id);
+ 
+       
+ 
+         $job->intitule= $request->input('intitule');
+         $job->domaine = $request->input('domaine');
+ 
+         $job->diplome= $request->input('diplome');
+         $job->lieu = $request->input('lieu');
+         $job->experience = $request->input('experience');
+         $job->competence = $request->input('competence');
+         $job->remun = $request->input('remun');
+         $job->description = $request->input('description');
+         $job->depot= $request->input('depot');
+         $job->type = $request->input('type');
+         $job->debut= $request->input('debut');
+         $job->delai = $request->input('delai');
+
+         
+         $job->save();
+ 
+         return redirect()->back();
+     }
+     public function destroy(Request $request,$id){
+        $job= Job::find($id);
+        $job->delete();
+        return redirect()->back();
+
+
+    }
+    public function indexjob(Request $request, $id) {
+
+        $job = Job::find($id);
+        $offre=$job->id;
+       $cvs=Cv::where('user_id',Auth::user()->id)->get();
+if(Auth::user()->admin == 0){
+        return view('jobdetails', ['job' => $job,'info'=> Auth::user(),'cvs'=>$cvs]);}
+        else{   
+            $postuler = DB::table('postules')
+            ->join('users', 'users.id', '=', 'postules.user_id')
+            ->join('jobs', 'postules.job_id', '=', 'jobs.id')  
+            ->select('users.*', 'postules.*')
+            ->where('postules.job_id', '=', $offre)
+            ->get();
+            
+
+            
+            return view('offredetails', ['job' => $job,'info'=> Auth::user(),'cvs'=>$cvs,'postuler'=>$postuler]);}
+    }
+
+    public function barrerecherche(Request $request) {
+$q=$request->get('ville');
+$p=$request->get('job');
+$users = DB::table('jobs')->join('companies', 'jobs.user_id', '=', 'companies.id')
+->where('lieu','=',$q)->orwhere('domaine','=',$p)
+->paginate(5);
+    
+    return view('joblist', ['offres' => $users]);
+    }
+    public function accueil() {
+       
+            $jobs=Job::All();
+            return view('acceuil', ['offres' => $jobs]);
+            }
 }
