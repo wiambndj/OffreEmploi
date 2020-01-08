00ba623784c1670fcf9ed4e4d70096915b0c2792
wiam bendjelloul <wiambndj97@gmail.com>
2020-01-08 22:51:30 +0100

    ajouter model cv

---
+++ app/Cv.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cv extends Model
+{
+    //
+ 
+    public function user() {
+    	return $this->belongsTo('App\User');
+    }
+
+
+    public function experiences() {
+    	return $this->hasMany('App\Experience');
+    }
+    public function job() {
+    	return $this->hasMany('App\Job');
+    }
+}
